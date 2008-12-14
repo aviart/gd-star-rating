5c8fd1629caf1665823b5913673c2263e0cef809
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2008-12-14 19:11:56 +0000

    - support for wordpress 2.7 add_meta_box - fixed comment review edit styles loading

--- integrate/edit27.php
+++
@@ -1,9 +0,0 @@
-<div class="meta-box-sortables ui-sortable" style="position: relative;">
-<div class="postbox">
-	<h3 class="hndle">GD Star Rating</h3>
-
-	<div class="inside" id="gdstarratingstuff">
-		<?php include(STARRATING_PATH.'integrate/edit.php'); ?>
-	</div>
-</div>
-</div>
