c4bc0db4bedc2d4f058e8c5e38bc0b375e544464
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-01-06 13:55:15 +0000

    - new official website url

--- trunk/gd-star-rating.php
+++ trunk/gd-star-rating.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: GD Star Rating
-Plugin URI: http://wp.gdragon.info/plugin/gd-star-rating/
+Plugin URI: http://www.gdstarrating.com/
 Description: Star Rating plugin allows you to set up rating system for pages and/or posts in your blog.
 Version: 1.0.8
 Author: Milan Petrovic
