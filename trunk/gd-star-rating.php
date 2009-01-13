7943f92707c2f8cab854316e46fc7e90280fa474
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-01-13 20:04:55 +0000

    - small updates

--- trunk/gd-star-rating.php
+++ trunk/gd-star-rating.php
@@ -116,8 +116,8 @@ if (!class_exists('GDStarRating')) {
         var $default_options = array(
             "version" => "1.0.9",
             "date" => "2009.01.13.",
-            "status" => "Beta",
-            "build" => 378,
+            "status" => "Stable",
+            "build" => 379,
             "news_feed_active" => 1,
             "debug_active" => 0,
             "debug_inline" => 1,
