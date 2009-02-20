b95a0f9b5ed01e150878febc44cc9f16ae96de6c
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-02-20 11:37:44 +0000

    - fixed rendering of multi review block - added multi review editor buttons

--- trunk/integrate/edit_multi.php
+++ trunk/integrate/edit_multi.php
@@ -3,15 +3,20 @@
     if ($multi_id == 0)
         _e("You first need to select which multi set you want to use for review rating.");
     else {
-        echo '<table width="100%" cellspacing="0" cellpadding="0"><tr><td width="35%">';
-        echo __("Multi Set ID").": <strong>".$multi_id."</strong><br />";
-        echo __("Multi Set Name").": <strong>".$set->name."</strong>";
+        echo '<table width="100%" cellspacing="0" cellpadding="0"><tr><td width="35%" class="gdsr-mur-review-info">';
+        echo __("Set ID").": <strong>".$multi_id."</strong><br />";
+        echo __("Set Name").": <strong>".$set->name."</strong>";
         echo '<div class="gdsr-table-split-edit"></div>';
-        echo __("");
-        echo '</td><td width="65%">';
 
+?>
+
+        <input type="button" class="gdsr-input-button" value="<?php _e("Clear"); ?>" />
+        <input type="button" class="gdsr-input-button" value="<?php _e("Revert"); ?>" />
+
+<?php
+
+        echo '</td><td width="65%" class="gdsr-mur-review-stars">';
         echo GDSRRender::multi_rating_review($votes, $post_id, $set, 20);
-
         echo '</td></tr></table>';
     }
     
