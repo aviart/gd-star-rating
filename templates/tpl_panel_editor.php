ef0b6e0cf19aa86bad4c50bc9db116b5f0787977
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-02-24 15:01:06 +0000

    - fixed prepare row title tag - new t2 templates panels

---
+++ templates/tpl_panel_editor.php
@@ -0,0 +1,7 @@
+<div class="wrap"><h2 class="gdptlogopage">GD Star Rating: <?php _e("Template Editor", "gd-star-rating"); ?></h2>
+<form method="post">
+<div class="gdsr">
+    
+</div>
+</form>
+</div>
