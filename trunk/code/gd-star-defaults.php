a809e2970860700da8a9f1f5ee36285f4df68e5d
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-04-11 01:23:18 +0000

    - fixed critical multi ratings saving bug

--- trunk/code/gd-star-defaults.php
+++ trunk/code/gd-star-defaults.php
@@ -5,7 +5,7 @@ class GDSRDefaults {
         "version" => "1.1.9",
         "date" => "2009.04.16.",
         "status" => "Stable",
-        "build" => 522,
+        "build" => 523,
         "external_javascript" => 0,
         "external_rating_css" => 1,
         "external_css" => 1,
