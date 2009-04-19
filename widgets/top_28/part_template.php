19ac434d6af25116b828b58a0da9db16953aad43
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-04-19 16:13:55 +0000

    - top rating widget for wp28

--- widgets/top_28/part_template.php
+++ widgets/top_28/part_template.php
@@ -1,17 +1,17 @@
-<input type="hidden" id="gdstarr-divtemplate[<?php echo $wpnm; ?>]" name="<?php echo $wpfn; ?>[div_template]" value="<?php echo $instance['div_template'] ?>" />
-<div id="gdstarr-divtemplate-off[<?php echo $wpnm; ?>]" style="display: <?php echo $instance['div_template'] == '1' ? 'none' : 'block' ?>">
+<input type="hidden" id="<?php echo $this->get_field_id('div_template'); ?>" name="<?php echo $this->get_field_name('div_template'); ?>" value="<?php echo $instance['div_template'] ?>" />
+<div id="<?php echo $this->get_field_id('div_template'); ?>-off" style="display: <?php echo $instance['div_template'] == '1' ? 'none' : 'block' ?>">
 <table border="0" cellpadding="2" cellspacing="0" width="100%" style="margin-bottom: 10px">
   <tr>
-    <td width="100" valign="top"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('gdstarr-divtemplate', '<?php echo $wpnm; ?>')"><?php _e("Template", "gd-star-rating"); ?></a></strong></td>
+    <td width="100" valign="top"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('<?php echo $this->get_field_id('div_template'); ?>')"><?php _e("Template", "gd-star-rating"); ?></a></strong></td>
     <td align="right"><?php _e("Click on the header title to display the options.", "gd-star-rating"); ?></td>
   </tr>
 </table>
 </div>
-<div id="gdstarr-divtemplate-on[<?php echo $wpnm; ?>]" style="display: <?php echo $instance['div_template'] == '1' ? 'block' : 'none' ?>">
+<div id="<?php echo $this->get_field_id('div_template'); ?>-on" style="display: <?php echo $instance['div_template'] == '1' ? 'block' : 'none' ?>">
 <table border="0" cellpadding="2" cellspacing="0" width="100%">
   <tr>
-    <td width="100"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('gdstarr-divtemplate', '<?php echo $wpnm; ?>')"><?php _e("Template", "gd-star-rating"); ?></a></strong></td>
-    <td align="right"><input class="widefat" style="width: 295px" type="text" name="<?php echo $wpfn; ?>[template]" id="gdstarr-tplitem" value="<?php echo wp_specialchars($instance["template"]); ?>" /></td>
+    <td width="100"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('<?php echo $this->get_field_id('div_template'); ?>')"><?php _e("Template", "gd-star-rating"); ?></a></strong></td>
+    <td align="right"><input class="widefat" style="width: 295px" type="text" name="<?php echo $this->get_field_name('template'); ?>" id="<?php echo $this->get_field_id('template'); ?>" value="<?php echo wp_specialchars($instance["template"]); ?>" /></td>
   </tr>
 </table>
 <table border="0" cellpadding="2" cellspacing="0" width="100%">
@@ -20,19 +20,19 @@
     <td colspan="2"><div class="gdsr-table-split-filter"></div></td>
   </tr>
 </table>
-<input type="hidden" id="gdstarr-divelemets[<?php echo $wpnm; ?>]" name="<?php echo $wpfn; ?>[div_elements]" value="<?php echo $instance['div_elements'] ?>" />
-<div id="gdstarr-divelemets-off[<?php echo $wpnm; ?>]" style="display: <?php echo $instance['div_elements'] == '1' ? 'none' : 'block' ?>">
+<input type="hidden" id="<?php echo $this->get_field_id('div_elements'); ?>" name="<?php echo $wpfn; ?>[div_elements]" value="<?php echo $instance['div_elements'] ?>" />
+<div id="<?php echo $this->get_field_id('div_elements'); ?>-off" style="display: <?php echo $instance['div_elements'] == '1' ? 'none' : 'block' ?>">
 <table border="0" cellpadding="2" cellspacing="0" width="100%" style="margin-bottom: 10px">
   <tr>
-    <td width="100" valign="top"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('gdstarr-divelemets', '<?php echo $wpnm; ?>')"><?php _e("Elements", "gd-star-rating"); ?></a></strong></td>
+    <td width="100" valign="top"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('<?php echo $this->get_field_id('div_elements'); ?>')"><?php _e("Elements", "gd-star-rating"); ?></a></strong></td>
     <td align="right"><?php _e("Click on the header title to display the options.", "gd-star-rating"); ?></td>
   </tr>
 </table>
 </div>
-<div id="gdstarr-divelemets-on[<?php echo $wpnm; ?>]" style="display: <?php echo $instance['div_elements'] == '1' ? 'block' : 'none' ?>">
+<div id="<?php echo $this->get_field_id('div_elements'); ?>-on" style="display: <?php echo $instance['div_elements'] == '1' ? 'block' : 'none' ?>">
 <table border="0" cellpadding="2" cellspacing="0" width="100%" style="margin-bottom: 10px">
   <tr>
-    <td width="100" valign="top"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('gdstarr-divelemets', '<?php echo $wpnm; ?>')"><?php _e("Elements", "gd-star-rating"); ?></a></strong></td>
+    <td width="100" valign="top"><strong><a style="text-decoration: none" href="javascript:gdsrShowHidePreview('<?php echo $this->get_field_id('div_elements'); ?>')"><?php _e("Elements", "gd-star-rating"); ?></a></strong></td>
     <td><strong>%RATING%</strong></td><td> : <?php _e("article rating", "gd-star-rating"); ?></td>
   </tr>
   <tr>
