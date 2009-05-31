68f36f8e238cc79f14647f44a0411fda140eb92f
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-05-31 23:47:48 +0000

    - reorganized tinymce plugin - added builder panel - fixed some visual bugs

--- tinymce3/panels/articlesrater.php
+++ tinymce3/panels/articlesrater.php
@@ -1,16 +1,3 @@
-<div id="articlesreview_panel" class="panel">
-<fieldset>
-<legend><?php _e("Articles Review Rating", "gd-star-rating"); ?></legend>
-<p><?php _e("StarReview will render stars representing review value assigned to the post or page.", "gd-star-rating"); ?></p>
-</fieldset>
-
-<fieldset>
-<legend><?php _e("Template", "gd-star-rating"); ?></legend>
-    <?php GDSRHelper::render_templates_section("RSB", "srTemplateRSB", 0, 300); ?>
-</fieldset>
-</div>
-
-<div id="articlesrater_panel" class="panel">
 <fieldset>
 <legend><?php _e("Articles Rating Block", "gd-star-rating"); ?></legend>
 <p><?php _e("StarRater will render actual rating block if you choose not to have it automatically inserted. This way you can position it wherever you want in the contnents.", "gd-star-rating"); ?></p>
@@ -25,4 +12,3 @@
 <legend><?php _e("Settings", "gd-star-rating"); ?></legend>
     <input type="checkbox" size="5" id="srArticleRead" name="srArticleRead" /><label for="srArticleRead"> <?php _e("Display rating block as read only.", "gd-star-rating"); ?></label><br />
 </fieldset>
-</div>
