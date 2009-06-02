e7aced614111c61e15be7a1bd6c1cf182da4a619
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-06-02 23:48:45 +0000

    - release: 1.4.2

---
+++ tags/1.4.2/code/js/main.php
@@ -0,0 +1,32 @@
+<?php if ($nonce != "") $nonce = sprintf("_ajax_nonce: '%s', ", $nonce); ?>
+
+function gdsrWait(rater, loader) {
+    jQuery("#"+rater).css("display", "none");
+    jQuery("#"+loader).css("display", "block");
+}
+
+function gdsrEmpty() { }
+
+<?php if ($include_mur_rating) include(STARRATING_PATH.'code/js/multi_fn.php'); ?>
+jQuery(document).ready(function() {
+    if (jQuery.browser.msie) jQuery(".gdsr_rating_as > a").attr("href", "javascript:gdsrEmpty()");
+    jQuery(".gdsr_rating_as > a").click(function() {
+        var el = jQuery(this).attr("id").split("X");
+        gdsrWait(el[5], el[6]);
+        jQuery.getJSON('<?php echo STARRATING_AJAX; ?>', {<?php echo $nonce; ?>vote_id: el[1], vote_value: el[2], vote_type: el[4], vote_tpl: el[7] }, function(json) {
+            gdsrWait(el[6], el[5]);
+            if (json.status == 'ok') {
+                jQuery("#gdr_stars_" + el[4] + el[1]).html("");
+                jQuery("#gdr_vote_" + el[4] + el[1]).css("width", json.value);
+                jQuery("#gdr_text_" + el[4] + el[1]).addClass("voted");
+                jQuery("#gdr_text_" + el[4] + el[1]).html(json.rater);
+            }
+        });
+    });
+    <?php include(STARRATING_PATH.'code/js/integration.php'); ?>
+    <?php if ($include_mur_rating) {
+        include(STARRATING_PATH.'code/js/multi_rt.php');
+        include(STARRATING_PATH.'code/js/multi_in.php');
+    } ?>
+});
+
---
+++ tags/1.4.2/tinymce3/panels/multisreview.php
@@ -0,0 +1,59 @@
+<fieldset>
+<legend><?php _e("Template", "gd-star-rating"); ?></legend>
+    <?php GDSRHelper::render_templates_section("RMB", "srTemplateRMB", 0, 300); ?>
+</fieldset>
+
+<fieldset>
+<legend><?php _e("Multi Reviews", "gd-star-rating"); ?></legend>
+    <table border="0" cellpadding="2" cellspacing="0" width="100%">
+      <tr>
+        <td class="gdsrleft"><?php _e("Set", "gd-star-rating"); ?>:</td>
+        <td class="gdsrright">
+            <select id="srMultiReviewSet" name="srMultiReviewSet" style="width: 200px">
+                <option value="0"><?php _e("Default", "gd-star-rating"); ?></option>
+                <?php GDSRHelper::render_styles_select($gdst_multis, 1); ?>
+            </select>
+        </td>
+      </tr>
+    </table>
+</fieldset>
+
+<fieldset>
+<legend><?php _e("Elements Stars", "gd-star-rating"); ?></legend>
+    <table border="0" cellpadding="2" cellspacing="0" width="100%">
+      <tr>
+        <td class="gdsrleft"><?php _e("Set", "gd-star-rating"); ?>:</td>
+        <td class="gdsrright">
+            <select id="srStarsStyleMRREl" name="srStarsStyleMRREl">
+                <?php GDSRHelper::render_styles_select($gdsr_styles, 'oxygen'); ?>
+            </select>
+        </td>
+      </tr>
+      <tr>
+        <td class="gdsrleft"><?php _e("Size", "gd-star-rating"); ?>:</td>
+        <td class="gdsrright">
+            <?php GDSRHelper::render_star_sizes_tinymce("srStarsSizeMRREl"); ?>
+        </td>
+      </tr>
+    </table>
+</fieldset>
+
+<fieldset>
+<legend><?php _e("Average Stars", "gd-star-rating"); ?></legend>
+    <table border="0" cellpadding="2" cellspacing="0" width="100%">
+      <tr>
+        <td class="gdsrleft"><?php _e("Set", "gd-star-rating"); ?>:</td>
+        <td class="gdsrright">
+            <select id="srStarsStyleMRRAv" name="srStarsStyleMRRAv">
+                <?php GDSRHelper::render_styles_select($gdsr_styles, 'oxygen'); ?>
+            </select>
+        </td>
+      </tr>
+      <tr>
+        <td class="gdsrleft"><?php _e("Size", "gd-star-rating"); ?>:</td>
+        <td class="gdsrright">
+            <?php GDSRHelper::render_star_sizes_tinymce("srStarsSizeMRRAv"); ?>
+        </td>
+      </tr>
+    </table>
+</fieldset>
