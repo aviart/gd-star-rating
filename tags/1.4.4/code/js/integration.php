8239531aac8a85a859b14e65ac55951197cfd31b
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-06-17 22:18:35 +0000

    - critical bug, not usable for fresh install

--- tags/1.4.4/code/js/integration.php
+++
@@ -1,9 +0,0 @@
-
-if (jQuery.browser.msie) jQuery(".gdsr_integration > a").attr("href", "javascript:gdsrEmpty()");
-    jQuery(".gdsr_integration > a").click(function() {
-        var el = jQuery(this).attr("id").split("X");
-        var pid = "#" + jQuery(this).parent().attr("id");
-        var new_width = el[1] * el[2];
-        jQuery(pid + "_stars_rated").css("width", new_width + "px");
-        jQuery(pid + "_value").val(el[1]);
-    });
