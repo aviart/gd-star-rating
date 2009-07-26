7bf83a7d7e060ea17812e721b027d6310700486a
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-07-26 22:33:25 +0000

    - thumbs problem in ie with javascript

--- trunk/code/defaults.php
+++ trunk/code/defaults.php
@@ -6,7 +6,7 @@ class GDSRDefaults {
         "date" => "2009.07.26.",
         "status" => "Stable",
         "product_id" => "gd-star-rating",
-        "build" => 845,
+        "build" => 846,
         "admin_views" => 0,
         "ajax_jsonp" => 0,
         "wp_query_handler" => 1,
