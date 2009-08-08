70a3fa726d760029fa149945696ce081f1a2b5eb
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-08-08 10:03:40 +0000

    - german, serbian and ukrainian languages updated

--- code/defaults.php
+++ code/defaults.php
@@ -6,7 +6,7 @@ class GDSRDefaults {
         "date" => "2009.08.10.",
         "status" => "Stable",
         "product_id" => "gd-star-rating",
-        "build" => 864,
+        "build" => 868,
         "admin_views" => 0,
         "ajax_jsonp" => 0,
         "wp_query_handler" => 1,
