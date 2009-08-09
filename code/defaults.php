d83c27082c879af13e6b0b1f2bd4fc345d877a58
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-08-09 21:08:55 +0000

    - few minor changes

--- code/defaults.php
+++ code/defaults.php
@@ -3,10 +3,10 @@
 class GDSRDefaults {
     var $default_options = array(
         "version" => "1.6.0",
-        "date" => "2009.08.10.",
+        "date" => "2009.08.09.",
         "status" => "Stable",
         "product_id" => "gd-star-rating",
-        "build" => 868,
+        "build" => 870,
         "admin_views" => 0,
         "ajax_jsonp" => 0,
         "wp_query_handler" => 1,
