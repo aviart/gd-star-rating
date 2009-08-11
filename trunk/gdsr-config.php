8c464c8a85b5ae65c456912cb4677fa860630fcc
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-08-11 22:08:24 +0000

    - updated config access control - updated readme and info files

--- trunk/gdsr-config.php
+++ trunk/gdsr-config.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * Full path to wp-load file. Use only if the location of wp-content folder is changed.
+ *
+ * example: define('STARRATING_WPLOAD', '/home/path/to/wp-load.php');
+ */
+define('STARRATING_WPLOAD', '');
+
+/**
  * Minimal user level required to access all plugins panels except Front and Builder.
  */
 define('STARRATING_ACCESS_LEVEL', 9);
