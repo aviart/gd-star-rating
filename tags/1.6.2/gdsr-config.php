ed6932cbaf8b069f36257b5b29ffbd85044b1be0
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-08-17 17:19:25 +0000

    - release: 1.6.2

---
+++ tags/1.6.2/gdsr-config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Full path to wp-load file. Use only if the location of wp-content folder is changed.
+ *
+ * example: define('STARRATING_WPLOAD', '/home/path/to/wp-load.php');
+ */
+define('STARRATING_WPLOAD', '');
+
+/**
+ * Minimal user level required to access all plugins panels except Front and Builder.
+ */
+define('STARRATING_ACCESS_LEVEL', 9);
+
+/**
+ * Minimal user level required to access some of the plugins panels.
+ */
+define('STARRATING_ACCESS_LEVEL_BUILDER', 1);
+define('STARRATING_ACCESS_LEVEL_SETUP', 9);
+
+/**
+ * Main admin account user ID's with access to special Security panel. If not set, all administrators will have access to it. You can set it to single account or to list of accounts, separated by commas ('1,6,23,12').
+ */
+define('STARRATING_ACCESS_ADMIN_USERIDS', '0');
+
+?>
---
+++ tags/1.6.2/tinymce3/panels/articlesreview.php
@@ -0,0 +1,9 @@
+<fieldset>
+<legend><?php _e("Articles Review Rating", "gd-star-rating"); ?></legend>
+<p><?php _e("StarReview will render stars representing review value assigned to the post or page.", "gd-star-rating"); ?></p>
+</fieldset>
+
+<fieldset>
+<legend><?php _e("Template", "gd-star-rating"); ?></legend>
+    <?php gdTemplateHelper::render_templates_section("RSB", "srTemplateRSB", 0, 300); ?>
+</fieldset>
