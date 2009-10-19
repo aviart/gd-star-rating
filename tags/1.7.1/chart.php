30c7c2c9b03dd9752295187c31c81e962ce57e09
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-10-19 21:44:17 +0000

    - too many bugs

--- tags/1.7.1/chart.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once("./config.php");
-    $wpload = get_gdsr_wpload_path();
-    require($wpload);
-
-    global $gdsr;
-
-?>
--- tags/1.7.1/options/wizard.php
+++
@@ -1,5 +0,0 @@
-<div class="wrap"><h2 class="gdptlogopage">GD Star Rating: <?php _e("Wizard", "gd-star-rating"); ?></h2>
-<div class="gdsr">
-Work in progress. Expected for the GD Star Rating 2.0.
-</div>
-</div>
