18b2609399679f9516cf14cfea8dc9c2fbe6cd7e
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-10-26 01:47:38 +0000

    - some settings panel changes

--- trunk/options/settings/settings_integration.php
+++ trunk/options/settings/settings_integration.php
@@ -1,9 +1,4 @@
 <table class="form-table"><tbody>
-<tr><th scope="row"><?php _e("WP Query Object", "gd-star-rating"); ?></th>
-    <td>
-        <input type="checkbox" name="gdsr_wp_query_handler" id="gdsr_wp_query_handler"<?php if ($gdsr_options["wp_query_handler"] == 1) echo " checked"; ?> /><label style="margin-left: 5px;" for="gdsr_wp_query_handler"><?php _e("Expand WP Query with new rating specific sorting and filtering.", "gd-star-rating"); ?></label>
-    </td>
-</tr>
 <tr><th scope="row"><?php _e("Comment Integration", "gd-star-rating"); ?></th>
     <td>
         <input type="checkbox" name="gdsr_cmm_integration_replay_hide_review" id="gdsr_cmm_integration_replay_hide_review"<?php if ($gdsr_options["cmm_integration_replay_hide_review"] == 1) echo " checked"; ?> /><label style="margin-left: 5px;" for="gdsr_cmm_integration_replay_hide_review"><?php _e("Hide comment integrated rating block if the comment is in reply mode (WP 2.7 or newer only).", "gd-star-rating"); ?></label>
@@ -70,64 +65,6 @@
         <input type="checkbox" name="gdsr_integrate_post_edit_mur" id="gdsr_integrate_post_edit_mur"<?php if ($gdsr_options["integrate_post_edit_mur"] == 1) echo " checked"; ?> /><label style="margin-left: 5px;" for="gdsr_integrate_post_edit_mur"><?php _e("Add multi ratings box in the post/page edit.", "gd-star-rating"); ?></label>
         <br />
         <input type="checkbox" name="gdsr_integrate_tinymce" id="gdsr_integrate_tinymce"<?php if ($gdsr_options["integrate_tinymce"] == 1) echo " checked"; ?> /><label style="margin-left: 5px;" for="gdsr_integrate_tinymce"><?php _e("Add rating shortcode plugin into tinyMCE visual editor.", "gd-star-rating"); ?></label>
-    </td>
-</tr>
-<tr><th scope="row"><?php _e("RSS Feeds", "gd-star-rating"); ?></th>
-    <td>
-        <input type="checkbox" name="gdsr_integrate_rss_powered" id="gdsr_integrate_rss_powered"<?php if ($gdsr_options["integrate_rss_powered"] == 1) echo " checked"; ?> /><label style="margin-left: 5px;" for="gdsr_integrate_rss_powered"><?php _e("Add small 80x15 badge to posts in RSS feed.", "gd-star-rating"); ?></label>
-        <br />
-        <input type="checkbox" name="gdsr_rss" id="gdsr_rss"<?php if ($gdsr_options["rss_active"] == 1) echo " checked"; ?> /><label style="margin-left: 5px;" for="gdsr_rss"><?php _e("Add ratings to posts in RSS feeds.", "gd-star-rating"); ?></label>
-        <div class="gdsr-table-split"></div>
-        <table cellpadding="0" cellspacing="0" class="previewtable">
-            <tr>
-                <td width="150"><?php _e("Data source", "gd-star-rating"); ?>:</td>
-                <td align="left">
-                <select style="width: 180px;" name="gdsr_rss_datasource" id="gdsr_rss_datasource">
-                    <option value="standard"<?php echo $gdsr_options['rss_datasource'] == 'standard' ? ' selected="selected"' : ''; ?>><?php _e("Standard Rating", "gd-star-rating"); ?></option>
-                    <option value="multis"<?php echo $gdsr_options['rss_datasource'] == 'multis' ? ' selected="selected"' : ''; ?>><?php _e("Multi Rating", "gd-star-rating"); ?></option>
-                    <option value="thumbs"<?php echo $gdsr_options['rss_datasource'] == 'thumbs' ? ' selected="selected"' : ''; ?>><?php _e("Thumbs Rating", "gd-star-rating"); ?></option>
-                </select>
-                </td>
-            </tr>
-        </table>
-        <div class="gdsr-table-split"></div>
-        <table cellpadding="0" cellspacing="0" class="previewtable">
-            <tr>
-                <td width="150"><?php _e("Stars", "gd-star-rating"); ?>:</td>
-                <td width="200" align="left">
-                <select style="width: 180px;" name="gdsr_rss_style" id="gdsr_rss_style">
-                <?php GDSRHelper::render_styles_select($gdsr_gfx->stars, $gdsr_options["rss_style"]); ?>
-                </select>
-                </td>
-                <td width="10"></td>
-                <td width="150" align="left">
-                <?php GDSRHelper::render_star_sizes("gdsr_rss_size", $gdsr_options["rss_size"]); ?>
-                </td>
-            </tr>
-            <tr>
-                <td width="150"><?php _e("Thumbs", "gd-star-rating"); ?>:</td>
-                <td width="200" align="left">
-                <select style="width: 180px;" name="gdsr_thumb_rss_style" id="gdsr_thumb_rss_style">
-                <?php GDSRHelper::render_styles_select($gdsr_gfx->thumbs, $gdsr_options["thumb_rss_style"]); ?>
-                </select>
-                </td>
-                <td width="10"></td>
-                <td width="150" align="left">
-                <?php GDSRHelper::render_thumbs_sizes("gdsr_thumb_rss_size", $gdsr_options["thumb_rss_size"]); ?>
-                </td>
-            </tr>
-        </table>
-        <div class="gdsr-table-split"></div>
-        <table cellpadding="0" cellspacing="0" class="previewtable">
-            <tr>
-                <td width="150"><?php _e("Rating Header", "gd-star-rating"); ?>:</td>
-                <td><input type="text" name="gdsr_rss_header_text" id="gdsr_rss_header_text" value="<?php echo wp_specialchars($gdsr_options["rss_header_text"]); ?>" style="width: 350px" /></td>
-            </tr>
-            <tr>
-                <td width="150"><?php _e("Rating Template", "gd-star-rating"); ?>:</td>
-                <td align="left"><?php gdTemplateHelper::render_templates_section("SSB", "gdsr_default_ssb_template", $gdsr_options["default_ssb_template"], 350); ?></td>
-            </tr>
-        </table>
     </td>
 </tr>
 </tbody></table>
