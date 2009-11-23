e5e1192d713d698e4296110f032f92c630a827b1
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2009-11-23 02:32:52 +0000

    - critical bug

--- tags/1.7.6/options/security.php
+++
@@ -1,5 +0,0 @@
-<div class="wrap"><h2 class="gdptlogopage">GD Star Rating: <?php _e("Security", "gd-star-rating"); ?></h2>
-<div class="gdsr">
-Work in progress. Expected for the GD Star Rating 1.8.
-</div>
-</div>
--- tags/1.7.6/widgets/widget_top.php
+++
@@ -1,4 +0,0 @@
-<?php include(STARRATING_PATH.'widgets/top/part_basic.php'); ?>
-<?php include(STARRATING_PATH.'widgets/top/part_filter.php'); ?>
-
-<input type="hidden" id="gdstarr-submit" name="<?php echo $wpfn; ?>[submit]" value="1" />
