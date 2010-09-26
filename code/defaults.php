40b941b2109e9b4c43be3b6ad555d7e3a1a6d161
milan@gdragon.info <milan@gdragon.info@93639cd2-8dbe-11dd-a205-0939b8ba2825>
2010-09-26 15:59:19 +0000

    - release changes

--- code/defaults.php
+++ code/defaults.php
@@ -8,7 +8,7 @@ class GDSRDefaults {
         "date" => "2010.09.26.",
         "status" => "Stable",
         "product_id" => "gd-star-rating",
-        "build" => 1104,
+        "build" => 1105,
         "admin_views" => 0,
         "bot_message" => "normal",
         "google_rich_snippets_format" => "microformat",
